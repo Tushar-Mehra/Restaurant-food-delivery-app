94f6527a7f558b0bd87252800250f1edd0894e31
Tushar <mehratushar384@gmail.com>
2025-06-22 22:20:23 +0530

    Switched from bcrypt to bcryptjs for Render deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,4 +2,4 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://Greatestack:9368149600@cluster0.uspn9dt.mongodb.net/food-del').then(()=>console.log("DB.Connected"));
-}
+}   
